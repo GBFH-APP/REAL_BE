b159de01ce7f19ff4cce7dc76962c8c40ddf7175
7beunseo <kimes0403@gmail.com>
2024-09-13 15:59:27 +0900

    ✨ [FEAT] 모든 Board 조회 API 작성 #7

---
+++ src/main/java/GBFH/GBFH_BE/repository/BoardConfigRepository.java
@@ -0,0 +1,8 @@
+package GBFH.GBFH_BE.repository;
+
+import GBFH.GBFH_BE.entity.BoardConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardConfigRepository extends JpaRepository<BoardConfig, String> {
+
+}
---
+++ src/main/java/GBFH/GBFH_BE/service/BoardConfigService.java
@@ -0,0 +1,24 @@
+package GBFH.GBFH_BE.service;
+
+import GBFH.GBFH_BE.dto.boardconfig.BoardConfigDTO;
+import GBFH.GBFH_BE.entity.BoardConfig;
+import GBFH.GBFH_BE.repository.BoardConfigRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class BoardConfigService {
+    private final BoardConfigRepository boardConfigRepository;
+
+    public List<BoardConfigDTO> getAllBoardConfig() {
+        List<BoardConfig> boardConfigList = boardConfigRepository.findAll();
+
+        return boardConfigList.stream()
+                .map(BoardConfigDTO::mapEntityToDTO)
+                .collect(Collectors.toList());
+    }
+}
