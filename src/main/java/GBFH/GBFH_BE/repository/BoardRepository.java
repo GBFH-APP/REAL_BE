8d3a80884216d1db7643d4257b12f629bf0afc14
7beunseo <kimes0403@gmail.com>
2024-09-16 21:12:53 +0900

    ✨ [FEAT] 분실물 생성 API 작성 #9

--- src/main/java/GBFH/GBFH_BE/repository/BoardRepository.java
+++ src/main/java/GBFH/GBFH_BE/repository/BoardRepository.java
@@ -3,6 +3,7 @@ package GBFH.GBFH_BE.repository;
 import GBFH.GBFH_BE.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
@@ -12,7 +13,17 @@ import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
     Optional<Board> findByIdx(Long idx);
+
     List<Board> findAllByBoardIdAndNotiAndNotiEndAfterAndNotiStartBefore(String boardId, Integer noti, String now, String today);
 
+    // idx 필드의 최대값을 조회
+    @Query("SELECT COALESCE(MAX(e.idx), 0) FROM Board e")
+    Long findMaxIdx();
+
+    // grp 필드의 최대값을 조회
+    @Query("SELECT COALESCE(MAX(e.grp), 0) FROM Board e")
+    Long findMaxGrp();
 }
+
