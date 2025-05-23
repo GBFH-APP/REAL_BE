66dc80433227446a128f1299f361de2ee33421f3
이규민 <kyumin991105@duksung.ac.kr>
2025-05-24 05:03:47 +0900

    🐛 [FIX] 모집 완료 필터링 재구현

--- src/main/java/GBFH/GBFH_BE/repository/LectureRepository.java
+++ src/main/java/GBFH/GBFH_BE/repository/LectureRepository.java
@@ -3,6 +3,7 @@ package GBFH.GBFH_BE.repository;
 import GBFH.GBFH_BE.entity.Lecture;
 import GBFH.GBFH_BE.entity.LectureGetDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,11 +12,19 @@ import java.util.Optional;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, String> {
-    List<Lecture> findAllByOpenAndRegIngOrderByCreateDtDesc(char open, char regIng);
+    List<Lecture> findAllByOpenAndRegIngAndYearOrderByCreateDtDesc(Character open, Character regIng, String year);
 
     @Override
     Optional<Lecture> findById(String s);
 
     List<Lecture> findAllByOpenAndRegIngAndStartDtGreaterThanEqualAndEndDtLessThanEqual(
             Character open, Character regIng, LocalDate startDt, LocalDate endDt);
+
+    @Query(value = """
+    SELECT *
+    FROM t_lecture
+    WHERE STR_TO_DATE(CONCAT(REPLACE(reg_end_ymd, '-', ''), LPAD(reg_end_hh, 2, '0'), LPAD(reg_end_mm, 2, '0')), '%Y%m%d%H%i')
+          < DATE_FORMAT(NOW(), '%Y%m%d%H%i')
+    """, nativeQuery = true)
+    List<Lecture> findLecturesWithRecruitEnded();
 }
