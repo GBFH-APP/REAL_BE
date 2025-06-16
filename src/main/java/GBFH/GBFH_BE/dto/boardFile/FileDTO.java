c744d26e17858a5264fd44302435ee7f2c50b9d3
이규민 <kyumin991105@duksung.ac.kr>
2025-06-17 00:28:54 +0900

    🐛 [FIX] filemapper에서 잘못 매칭됨

--- src/main/java/GBFH/GBFH_BE/dto/boardFile/FileDTO.java
+++ src/main/java/GBFH/GBFH_BE/dto/boardFile/FileDTO.java
@@ -9,9 +9,35 @@ import lombok.Getter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Builder
 public class FileDTO {
     private Long id;
     private String uri; // 현재는 파일 이름만 넘겨주는데 나중에 연동할 때는 앞에 uri 붙여줄게여
+
+    public static FileDTO toDTO(BoardFile boardFile) {
+        return FileDTO.builder()
+                .id(boardFile.getIdx())
+                .uri(boardFile.getFileId())
+                .build();
+    }
+    public static FileDTO toDTO(ImageFile imageFile) {
+        return FileDTO.builder()
+                .id(imageFile.getIdx())
+                .uri(imageFile.getFileId())
+                .build();
+    }
+
 }
